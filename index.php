d3fb5fee9de2764c48bafa03325b7341b50964c2
ChedIz <20231000363@feualabang.edu.ph>
2025-06-06 20:30:12 +0800

    create of files and initial header of project

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Student Grade Calculator</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Student Grade Calculator</h1>
+    <p>This system will compute grades for 5 students based on your input.</p>
+    <form action="students.php" method="post">
+       
+    </form>
+</body>
+</html>
---
+++ student.php
@@ -0,0 +1,3 @@
+<?php
+// this file will be coded later
+?>
